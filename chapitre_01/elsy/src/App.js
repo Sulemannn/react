59f654d2701efc26f418a183c278a17f15247e98
unknown <suleman.khn10@gmail.com>
2021-04-09 17:42:07 +0200

    elsy etape 5

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import './App.css';
 
-import Steps from "./components/Steps.jsx"
+import Box from './components/Box.jsx'
+
+import './styles/global.css'
 
 const tempMin = -20;
 const tempMax = 40;
@@ -15,13 +17,32 @@ class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="row"><Steps />
+        <div className="row">
 
+          {/* Water */}
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" ></Box >
+          
+          {/* Steps */}
+          <Box icon="directions_walk" color="black" value={3000} unit="steps" ></Box >
+
+          {/* Heart */}
+          <Box icon="favorite" color="red" value={120} unit="bpm" ></Box >
+
+          {/* Température */}
+          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" ></Box >
+
+
+
+
+        
+
+          
           <p>Heart : {heartMin}</p>
           <p>Temperature : {tempMin}</p>
           <p>Steps : {stepsMin}</p>
 
         </div>
+       
         
       </div>
     );
---
+++ chapitre_01/elsy/src/components/Box.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import '../styles/global.css'
+
+class Box extends React.Component {
+
+  render() {
+    return (
+
+      <div className="box col-sm-3 col-6">
+
+        <span class="material-icons" style={{fontSize:"100", color: this.props.color }}>{this.props.icon}</span>
+        <p>{this.props.value} {this.props.unit}</p>
+
+
+      </div>
+
+
+    );
+  }
+}
+
+export default Box;
