60390d98b796470e15b8618fc41291c584d83d06
unknown <suleman.khn10@gmail.com>
2021-04-14 15:42:14 +0200

    form

---
+++ chapitre_04/form/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Form from './components/Form'
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <Form />
+            </div>
+        )
+    }
+}
+
+
+export default App;
---
+++ chapitre_04/form/src/components/Form.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Form extends React.Component {
+    render () {
+        return (
+            
+            <form>
+                <div>
+                <label>Username</label>
+                <input type="text"/>
+
+                </div>
+                
+                
+            </form>
+        )
+    }
+}
+
+export default Form
