b9b3677b35f6476957034ba1ffcb03fc5dd2aed1
Suleman <suleman.khn10@gmail.com>
2021-04-28 16:04:37 +0200

    Countries etape 1

--- chapitre_05/countries/src/App.js
+++ chapitre_05/countries/src/App.js
@@ -3,16 +3,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
 
-  constructor () {
-    super ()
+  constructor() {
+    super()
+
+    this.state = {
+      name: "",
+      capital: "",
+      flag: "",
+      population: "",
+      region: ""
+    };
 
   }
 
-  render () {
+  componentDidMount() {
+    fetch("https://restcountries.eu/rest/v2/name/france")
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+
+        this.setState({
+          name: result[0].name,
+          capital: result[0].capital,
+          flag: result[0].flag,
+          population: result[0].population,
+          region: result[0].region
+        });
+      })
+      .catch(err => console.error("There is an error !", err));
+  }
+
+  render() {
     return (
-      <h1>
-        Hello world
-      </h1>
+
+      <div className="d-flex flex-column">
+
+        <span>name : {this.state.name}</span>
+        <span>capital : {this.state.capital}</span>
+        <span>flag : {this.state.flag}</span>
+        <span>population : {this.state.population}</span>
+        <span>region : {this.state.region}</span>
+
+      </div>
+
     )
   }
 }
