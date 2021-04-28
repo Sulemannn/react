9a5e4dacecda3bc0d2d7ea672d3184e388918cf5
Suleman <suleman.khn10@gmail.com>
2021-04-28 17:36:23 +0200

    countries etape 2

--- chapitre_05/countries/src/App.js
+++ chapitre_05/countries/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "./component/Button";
 
 class App extends React.Component {
 
@@ -12,13 +13,29 @@ class App extends React.Component {
       flag: "",
       population: "",
       region: ""
-    };
+    }
 
+    this.getCountry = this.getCountry.bind(this)
+  }
+
+  getCountry(country) {
+    fetch(`https://restcountries.eu/rest/v2/name/${country.toLowerCase()}`)
+      .then(res => res.json())
+      .then(result => {
+        
+        this.setState({
+          name: result[0].name,
+          capital: result[0].capital,
+          flag: result[0].flag,
+          population: result[0].population,
+          region: result[0].region
+        })
+      })
   }
 
   componentDidMount() {
     fetch("https://restcountries.eu/rest/v2/name/france")
-      .then(response => response.json())
+      .then(res => res.json())
       .then(result => {
         console.log(result)
 
@@ -28,24 +45,37 @@ class App extends React.Component {
           flag: result[0].flag,
           population: result[0].population,
           region: result[0].region
-        });
-      })
-      .catch(err => console.error("There is an error !", err));
+        })
+      }
+      )
+  }
+
+  buttonRender() {
+
+    return (
+      <div>
+        <Button onClick={this.getCountry}>France</Button>
+        <Button onClick={this.getCountry} >Brazil</Button>
+        <Button onClick={this.getCountry} >Croatia</Button>
+      </div>
+    )
   }
 
   render() {
+
+
     return (
-
       <div className="d-flex flex-column">
-
         <span>name : {this.state.name}</span>
         <span>capital : {this.state.capital}</span>
         <span>flag : {this.state.flag}</span>
         <span>population : {this.state.population}</span>
         <span>region : {this.state.region}</span>
 
-      </div>
+        {this.buttonRender()}
 
+        
+      </div>
     )
   }
 }
