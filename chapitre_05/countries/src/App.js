2fe1c85b79c936ea34b20441cb1fa5edcbb472a2
Suleman <suleman.khn10@gmail.com>
2021-04-28 15:13:13 +0200

    test

---
+++ chapitre_05/countries/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+
+  constructor () {
+    super ()
+
+  }
+
+  render () {
+    return (
+      <h1>
+        Hello world
+      </h1>
+    )
+  }
+}
+
+export default App;
