6c83b79d71bde70d844d0255d86cc1944ab511f3
Suleman <suleman.khn10@gmail.com>
2021-05-04 11:29:29 +0200

    bakery step 8..

--- chapitre_04/bakery/src/components/Button.jsx
+++ chapitre_04/bakery/src/components/Button.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 
 class Button extends Component {
-    
+
     render() {
         return (
             <button
-                onClick={this.props.onClick} 
-                className={this.props.isSelected === true?  "btn btn-primary" : "btn btn-light"}
-                type="button">
-                    {this.props.children}
+                onClick={this.props.onClick}
+                className={this.props.isSelected === true ? "btn btn-primary" : "btn btn-light"} type="button">
+                {this.props.children}
             </button>
         )
     }
--- chapitre_04/bakery/src/components/List.jsx
+++ chapitre_04/bakery/src/components/List.jsx
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 
 class List extends Component {
     render() {
-        return (
-            <ul>
-                {this.props.listItems.map(curr => {
-                    return ( <li>{curr.name} - {curr.price} </li>)
-                })}
-            </ul>
-        )
+
+        if (this.props.listItems.length === 0) {
+            return <h2>No items are available</h2>
+
+        } else {
+
+            return (
+                
+                <ul>
+                    {this.props.listItems.map(curr => {
+                        return (<li>{curr.name} : {curr.price} €</li>)
+                    })}
+                </ul>
+            )
+        }
     }
 }
 export default List;
